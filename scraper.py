34c1b03d010ff06661dead20dcf11621d2b98cd0
rimalh <rimalh@wwu.edu>
2025-03-18 21:58:42 -0700

    commit

--- scraper.py
+++ scraper.py
@@ -1,10 +1,12 @@
 # create a web scraper that will scrape the website https://realpython.github.io/fake-jobs/
 
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
+soup = BeautifulSoup(page.content, "html.parser")
 print(page.text)
 
 
